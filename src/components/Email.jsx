d54445e92226d2f1537c3d396f3e49949c4e8dd2
Sujal-Jariwala <jariwalasujal68@gmail.com>
2024-08-03 12:44:22 +0530

    modified and add build

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -49,9 +49,9 @@ function Email() {
   }, []);
 
   return (
-    <div className='flex justify-center items-center max-md:h-auto min-h-screen bg-gray-800 font-roboto'>
-      <div className='flex bg-white rounded-3xl overflow-hidden max-w-4xl max-md:flex-col-reverse max-md:w-[90%] px-8 py-4 max-md:p-0 max-md:justify-center max-md:items-center max-md:h-full font-roboto'>
-        <div className='flex-1 pr-6 ml-10 max-md:m-0'>
+    <div className='flex justify-center items-center min-h-screen bg-gray-800 font-roboto '>
+      <div className='flex bg-white rounded-3xl overflow-hidden max-w-4xl max-md:flex-col-reverse max-md:w-[100%] px-8 py-4 max-md:p-0 max-md:justify-center max-md:items-center max-md:h-auto font-roboto max-md:rounded-none'>
+        <div className='flex-1 pr-6 ml-10 max-md:m-0 max-md:px-4'>
           <h1 className='text-5xl font-extrabold mb-6 mt-14 text-[#242742] max-md:font-[30px]'>Stay updated!</h1>
           <p className='mb-6'>Join 60,000+ product managers receiving monthly updates on:</p>
           <div className='space-y-4 mb-8'>
@@ -69,7 +69,7 @@ function Email() {
             </div>
           </div>
           <div className='mb-4'>
-            <div className='flex justify-between items-center'>
+            <div className='flex justify-between items-center max-md:max-w-full'>
               <label htmlFor="email" className='block text-sm font-semibold mb-2'>Email Address</label>
               {errorMsg && (
                 <span className='text-tomato text-sm font-semibold font-roboto'>
@@ -83,18 +83,18 @@ function Email() {
               id="email"
               type='email'
               placeholder='email@company.com'
-              className={`px-4 py-3 w-full rounded-lg border-solid focus:border-black focus:border-solid focus:outline-none ${errorMsg ? 'border-2 border-tomato bg-red-200 placeholder:tomato' : 'border'}`}
+              className={`px-4 py-3 w-full rounded-lg border-solid focus:border-black focus:border-solid focus:outline-none max-md:max-w-full ${errorMsg ? 'border-2 border-tomato bg-red-200 placeholder:tomato' : 'border'}`}
             />
           </div>
           <button
             onClick={handleSubmit}
-            className='bg-[#242742] py-3 rounded-lg w-full text-white font-bold transition ease-in-out duration-300 hover:bg-gradient-to-r from-[#ff5fd4] to-[#ff5100] hover:shadow-2xl hover:shadow-pink-500 '
+            className='bg-[#242742] py-3 rounded-lg w-full text-white font-bold transition ease-in-out duration-300 hover:bg-gradient-to-r from-[#ff5fd4] to-[#ff5100] hover:shadow-2xl hover:shadow-pink-500 max-md:mb-10'
           >
             Subscribe to monthly newsletter
           </button>
         </div>
-        <div className='flex-1 rounded-xl overflow-hidden ml-11 max-md:m-0 max-md:w-full max-md:justify-center max-md:items-center max-md:align-middle'>
-          <img src={imageSrc} alt="Illustration" className='object-cover max-w-screen max-md:w-full' />
+        <div className='flex-1 rounded-xl overflow-hidden ml-11 max-md:m-0 max-md:w-full max-md:justify-center max-md:items-center max-md:align-middle max-md:rounded-none'>
+          <img src={imageSrc} alt="Illustration" className='object-cover max-w-screen max-md:w-full max-md:overflow-visible max-md:rounded-none' />
         </div>
       </div>
     </div>
